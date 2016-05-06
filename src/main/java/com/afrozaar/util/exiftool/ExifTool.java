931b158b1c04288c005a377ccded914c5eaca939
Freddie <freddieodonnell@afrozaar.com>
2016-05-06 19:17:35 +0200

    Fixing small bug with regard to returning empty strings and apssing them as allowed cases

--- src/main/java/com/afrozaar/util/exiftool/ExifTool.java
+++ src/main/java/com/afrozaar/util/exiftool/ExifTool.java
@@ -256,7 +256,9 @@ public class ExifTool implements IExifTool {
     private Optional<String> getFirstFieldValue(JsonNode profileNode, String... fieldKeys) {
         final Optional<JsonNode> first = Arrays.stream(fieldKeys)
                 .map(fieldKey -> Optional.ofNullable(profileNode.get(fieldKey)))
-                .filter(Optional::isPresent).map(Optional::get)
+                .filter(Optional::isPresent)
+                .filter(node -> !node.get().asText().equals(""))
+                .map(Optional::get)
                 .findFirst();
 
         return first.isPresent() ? Optional.of(first.get().asText()) : Optional.empty();
